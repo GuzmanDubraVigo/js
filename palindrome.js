c676e8df0593653c7d9010f19efc5a4276b0691a
Guzman Dubra <151534076+GuzmanDubraVigo@users.noreply.github.com>
2024-01-30 17:17:11 +0100

    palidrome

---
+++ palindrome.js
@@ -0,0 +1,30 @@
+//Calcula si una cadena de texto es un palindrome
+//Palabra o frase cuyas letras están dispuestas de tal manera que resulta la misma leída de izquierda a derecha que de derecha a izquierda
+function palindrome(texto) {
+    // Compruebo si "text" es un string
+    if (typeof texto === "string") {
+        const formattedText = texto.toLowerCase().replaceAll(' ', '');
+
+        // Recorro la cadena de texto
+        for ( let index=0;index < formattedText.length;index++) {
+            const firtIndex = index
+            const lastIndex = formattedText.length - index -1;
+            // si el indice inicial a sobrepasado el posterior
+            // hemos terminado: es un palindrome!!
+            if (firtIndex <= lastIndex){
+                return true;
+            }
+            
+            if (formattedText.charAt(firtIndex) !== formattedText.charAt(lastIndex)) {
+                return false;
+
+            }
+        }
+
+        return true;
+
+    }
+
+    return false;
+}
+console.log(palindrome("dabale arroz a la zorra el abad"));
