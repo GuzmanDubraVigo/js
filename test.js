77cdeb105ac22a25987ee9568d5da04e9dfc6fff
Guzman Dubra <151534076+GuzmanDubraVigo@users.noreply.github.com>
2024-02-01 16:04:13 +0100

    ok

--- test.js
+++ test.js
@@ -1,11 +1,2 @@
 console.clear();
 
-let greeting = 'Hello';
-let myName = 'Guzman';
-const contentElement = document.getElementById('content');
-
-contentElement.innerHTML = ` 
-<h1 id="title">
-    ${greeting + ' ' + myName + '!'}
-</h1>
-`;
