79a7a1f3da49a87f51523487756b28596b64a7e9
Guzman Dubra <151534076+GuzmanDubraVigo@users.noreply.github.com>
2024-02-02 20:52:09 +0100

    ok

--- test.js
+++ test.js
@@ -1,11 +1,13 @@
-console.clear();
-
-let greeting = 'Hello';
-let myName = 'Guzman';
-const contentElement = document.getElementById('content');
-
-contentElement.innerHTML = ` 
-<h1 id="title">
-    ${greeting + ' ' + myName + '!'}
-</h1>
-`;
+function sortStrings(a, b) {
+    const string1 = a.toLowerCase();
+    const string2 = b.toLowerCase();
+  
+    if (string1 < string2) return -1;
+    if (string1 > string2) return 1;
+    return 0;
+  }
+  
+  const arrayOfTitles = movies.map((movie) => movie.title);
+  const sortedArrayOfTitles = arrayOfTitles.sort(sortStrings);
+  
+  showContent(10, sortedArrayOfTitles);
